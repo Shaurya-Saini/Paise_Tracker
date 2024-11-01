3c8a4335e17cfa7de44501999c29b6087a7c6a8c
Shaurya-Saini <shauryathemaster01@gmail.com>
2024-11-01 15:54:21 +0530

    completing frontend

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.textinput import TextInput
+
 
 class Home(Screen):
     pass
@@ -24,7 +26,25 @@ class Exp_Page(Screen):
 
 
 class Inc_Page(Screen):
+    def add_income(self):
+        amount = self.ids.amt_inp.text
+        note = self.ids.note_inp.text
+        print("amt = ",amount,"\nnote = ",note)
+        self.ids.amt_inp.text = ""
+        self.ids.note_inp.text = ""
+
+class Display_Page(Screen):
     pass
+
+class Delete_Page(Screen):
+    def type_click(self, value):
+        self.ids.type_del.text= value
+    def delete_rec(self):
+        iden = self.ids.del_id.text
+        del_type = self.ids.type_del.text
+        print("type = ",del_type,"\nid = ",iden)
+        self.ids.del_id.text = ""
+        self.ids.type_del.text = "Type"
 
 class Rec_Page(Screen):
     pass
